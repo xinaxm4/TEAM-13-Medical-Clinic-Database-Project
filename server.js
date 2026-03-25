067a97fe31c3ccf60e42c496625eec0d1e7a8cde
xinaxm4 <tina.tran9106@yahoo.com>
2026-03-25 15:36:57 -0500

    Prep for Railway deployment: dynamic PORT, add .gitignore
    - server.js: use process.env.PORT || 3000 so Railway can inject its port
    - .gitignore: exclude node_modules, .env, logs, .DS_Store

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const patientRoutes   = require("./routes/patientRoutes");
 const locationsRoutes = require("./routes/locationsRoutes");
 
 const app  = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
